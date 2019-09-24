1f3ab0d36c941227799cc96ecea737115348cb8b
Harsh Agarwal <harshagr63@gmail.com>
2019-09-24 16:24:58 +0530

    Hello

--- app/src/androidTest/java/com/example/loginui/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/loginui/ExampleInstrumentedTest.java
@@ -3,6 +3,7 @@ package com.example.loginui;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
